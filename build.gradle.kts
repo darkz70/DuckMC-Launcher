b050482983dc7ae8f8024ff703140cb790bfe67b
darkz70 <d91313250@gmail.com>
2026-04-27 13:44:54 +0200

    Replace Kotlin plugin ID with alias in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
+    alias(libs.plugins.kotlin.android) apply false
